4ce2d428dac16f1ed148caf59f757be6bc40e963
juan bernal <mesieou@gmail.com>
2023-06-11 18:56:24 +1000

    added seeds for current month

---
+++ db/objectives_and_agendas.rb
@@ -0,0 +1,49 @@
+OBJECTIVES = [
+  [
+    "Highest Priority: Evaluate the potential benefits and drawbacks of adopting the new accounting software",
+    "Middle Priority: Determine the implementation requirements and timeline for the new accounting software",
+    "Low Priority: Establish a communication plan for stakeholders regarding the new accounting software"
+  ],
+  [
+    "Highest Priority: Evaluate the potential benefits and drawbacks of adopting the new accounting software",
+    "Middle Priority: Determine the implementation requirements and timeline for the new accounting software",
+    "Low Priority: Establish a communication plan for stakeholders regarding the new accounting software"
+  ],
+  [
+    "Highest Priority: Assess the readiness of the organization for adopting the new accounting software",
+    "Middle Priority: Identify the training needs and resources required for successful implementation",
+    "Low Priority: Define the key performance indicators to measure the effectiveness of the new accounting software"
+  ],
+  [
+    "Highest Priority: Determine the cost-benefit analysis of adopting the new accounting software",
+    "Middle Priority: Assess the impact on existing processes and workflows",
+    "Low Priority: Identify potential training needs and strategies for successful implementation"
+  ]
+]
+
+AGENDAS = [
+  [
+    "From 10:00 to 10:05 Welcome and Introductions (5 minutes)",
+    "From 10:05 to 10:25 Presentation on the New Accounting Software (20 minutes)",
+    "From 10:25 to 10:45 Group Discussion on Benefits and Drawbacks (20 minutes)",
+    "From 10:45 to 11:00 Q&A and Wrap-up (15 minutes)"
+  ],
+  [
+    "From 09:30 to 09:35 Introduction and Welcome (5 minutes)",
+    "From 09:35 to 09:45 Overview of the New Accounting Software (10 minutes)",
+    "From 09:45 to 09:55 Pros and Cons Discussion (10 minutes)",
+    "From 09:55 to 10:00 Next Steps and Conclusion (5 minutes)"
+  ],
+  [
+    "From 14:00 to 14:10 Opening Remarks and Introduction (10 minutes)",
+    "From 14:10 to 14:30 Presentation on the Benefits and Features of the New Accounting Software (20 minutes)",
+    "From 14:30 to 14:50 Discussion on Training and Resource Requirements (20 minutes)",
+    "From 14:50 to 15:00 Action Planning and Next Steps (10 minutes)"
+  ],
+  [
+    "From 13:30 to 13:35 Introduction and Meeting Overview (5 minutes)",
+    "From 13:35 to 13:55 Presentation on the New Accounting Software Features (20 minutes)",
+    "From 13:55 to 14:10 Discussion on Cost-Benefit Analysis (15 minutes)",
+    "From 14:10 to 14:25 Assessment of Process and Workflow Impact (15 minutes)"
+  ]
+]
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,66 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+require 'date'
+require_relative './objectives_and_agendas.rb'
+require_relative './transcript_summary_actions.rb'
+puts "cleaning users, users, meetings, bookings and videos..."
+
+User.destroy_all
+Meeting.destroy_all
+Booking.destroy_all
+Video.destroy_all
+
+now = DateTime.now
+minutes = [0, 15, 30, 45, 60, 90]
+start_of_month = DateTime.new(now.year, now.month, 1, 8, 0, 0)
+end_of_month = (start_of_month >> 1) - 1
+
+random_date_time_before_today = rand(start_of_month..now)
+random_date_time_after_today = rand(now..end_of_month)
+
+puts "creating users, videos, meetings, and bookings..."
+
+30.times do
+  user = User.create!(
+    name: Faker::Name.name,
+    password: "123456",
+    email: Faker::Internet.email,
+    job_title: Faker::Job.title,
+    mobile: Faker::PhoneNumber.cell_phone
+  )
+  30.times do
+    video = Video.create!(
+      start_date_time: random_date_time_before_today,
+      end_date_time: random_date_time_before_today + Rational(minutes.sample, 24 * 60),
+      transcript: TRANSCRIPT,
+      summary: SUMMARY,
+      actions: ACTIONS
+    )
+    meeting_before_today = Meeting.new(
+      start_date_time: random_date_time_before_today,
+      end_date_time: random_date_time_before_today + Rational(minutes.sample, 24 * 60),
+      title: Faker::Hacker.say_something_smart,
+      location: "Room: #{Faker::Color.color_name}",
+      objectives: OBJECTIVES.sample,
+      agenda: AGENDAS.sample,
+      video: video
+    )
+    Booking.create!(
+      user: user,
+      meeting: meeting_before_today,
+      status: "accepted"
+    )
+    meeting_after_today = Meeting.new(
+      start_date_time: random_date_time_after_today,
+      end_date_time: random_date_time_after_today + Rational(minutes.sample, 24 * 60),
+      title: Faker::Lorem.sentence(word_count: 3),
+      location: "Room: #{Faker::Color.color_name}",
+      objectives: OBJECTIVES.sample,
+      agenda: AGENDAS.sample,
+      video: video
+    )
+    Booking.create!(
+      user: user,
+      meeting: meeting_after_today,
+      status: "accepted"
+    )
+  end
+end
